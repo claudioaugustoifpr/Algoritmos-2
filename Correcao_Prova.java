83da443cbd6ffc8385029ba0033410b14fb82090
claudioaugustoifpr <20251pvai10030029@estudantes.ifpr.edu.br>
2026-03-24 22:22:07 -0300

    Create Correcao_Prova.java

---
+++ Correcao_Prova.java
@@ -0,0 +1,23 @@
+questao 1 
+
+  import java.util.Scanner;
+
+
+public class CorrecaoProva {
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a =sc.nextInt();
+		int b =sc.nextInt();
+		int soma = 0;
+		
+		for(int i = a; i <= b; i++) {
+			if(i%2 == 0) {
+				soma = soma + i;
+			}
+		}
+		System.out.println("A soma dos pares entre " +a+ " e " +b+ " é " +soma);
+	}
+}
+
+questao 2
